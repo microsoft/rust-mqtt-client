c126e7537923f8dca1a1008f0ff0c1876fbf2b99
Carter Tinney <catinney@microsoft.com>
2025-09-09 08:49:50 -0700

    lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,4 +20,4 @@ pub mod topic;
 // NOTE: Any dispatching or connection management would be supplementary components.
 // I am in favor of providing them, but they are built on top of these core components and would be optional.
 // These core components have been designed to enable the future use of such additional components,
-// e.g. AckToken is designed to support future usage in complex dispatching scenarios.
+// e.g. PubAckToken is designed to support future usage in complex dispatching scenarios.
