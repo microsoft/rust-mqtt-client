9746643df409607045b8c8a3e10cf6325db07297
Carter Tinney <catinney@microsoft.com>
2025-09-09 07:08:33 -0700

    missing headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! MQTT client library
+
+// TODO: Revisit the exposed API of these modules, and remove the linting suppressions as necessary
 pub mod buffer_pool;
 pub (crate) mod io;
 pub (crate) mod mqtt_proto;
