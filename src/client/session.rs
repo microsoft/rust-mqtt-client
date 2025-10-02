b4120313339877b0c20f0ea149f1da877aeeed97
Arnav Singh <arsing@microsoft.com>
2025-10-01 20:58:41 -0700

    Stub for Session::next_outgoing_packet()

--- src/client/session.rs
+++ src/client/session.rs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 use std::collections::{HashMap, VecDeque};
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::time::{Duration, Sleep};
 
 use crate::buffer_pool::Shared;
 use crate::client::{
@@ -14,8 +17,9 @@ use crate::client::{
     session::pkid::PkidPool,
 };
 use crate::mqtt_proto::{
-    ConnAck, Disconnect, Packet, PacketIdentifier, PubAck, PubRec, PubRel, Publish,
-    SessionExpiryInterval, SubAck, UnsubAck,
+    ConnAck, Connect, ConnectSessionExpiryInterval, Disconnect, KeepAlive, Packet,
+    PacketIdentifier, PacketIdentifierDupQoS, PingReq, PubAck, PubComp, PubRec, PubRel, Publish,
+    SessionExpiryInterval, SubAck, Subscribe, UnsubAck, Unsubscribe,
 };
 use crate::token::{
     ConnectCompletionNotifier, PubRecCompletionNotifier, PubRelCompletionNotifier,
@@ -68,6 +72,7 @@ impl<S: Shared> Session<S> {
             sub_rx,
             ack_rx,
             i_pub_tx,
+            pingreq: None,
         };
         Self {
             ch,
@@ -88,7 +93,143 @@ impl<S: Shared> Session<S> {
     /// Returns the next outgoing MQTT packet to be sent over the network
     #[allow(clippy::unused_self)]
     pub async fn next_outgoing_packet(&mut self) -> Option<Packet<S>> {
-        unimplemented!()
+        match (self.connected, &mut self.inflight.connect) {
+            // If we're currently disconnected, then only poll `conn_rx` and generate a `Connect`
+            (false, inflight_connect @ None) => {
+                let (notifier, _properties) = loop {
+                    match self.ch.conn_rx.recv().await? {
+                        ConnectionRequest::Connect(notifier, properties) => {
+                            break (notifier, properties);
+                        }
+                        // TODO: Just ignore it? Or return an error?
+                        // Or split conn_rx into separate channels for Connect and Disconnect requests?
+                        ConnectionRequest::Disconnect(..) => (),
+                    }
+                };
+                *inflight_connect = Some(notifier);
+                // TODO: Get values from properties
+                Some(Packet::Connect(Connect {
+                    username: None,
+                    password: None,
+                    will: None,
+                    client_id: None,
+                    clean_start: true,
+                    keep_alive: KeepAlive::Infinite,
+                    session_expiry_interval: ConnectSessionExpiryInterval(
+                        SessionExpiryInterval::Infinite,
+                    ),
+                    other_properties: Default::default(),
+                }))
+            }
+
+            // If we're currently disconnected and waiting for CONNACK, then yield `Pending`
+            (false, Some(_)) => std::future::pending().await,
+
+            // If we're currently connected, then poll self.ch
+            (true, _) => {
+                #[allow(unreachable_code)] // TODO: Remove when todo!()s are resolved
+                let packet = match poll_connected_channels(&mut self.ch).await {
+                    ConnectedChannelsOutgoingPacket::AcknowledgementRequest(ack_req) => {
+                        match ack_req {
+                            AcknowledgementRequest::PubAck(..) => Packet::PubAck(PubAck {
+                                packet_identifier: todo!(),
+                                reason_code: todo!(),
+                                other_properties: todo!(),
+                            }),
+
+                            AcknowledgementRequest::PubComp(..) => Packet::PubComp(PubComp {
+                                packet_identifier: todo!(),
+                                reason_code: todo!(),
+                                other_properties: todo!(),
+                            }),
+
+                            AcknowledgementRequest::PubRec(..) => Packet::PubRec(PubRec {
+                                packet_identifier: todo!(),
+                                reason_code: todo!(),
+                                other_properties: todo!(),
+                            }),
+
+                            AcknowledgementRequest::PubRel(..) => Packet::PubRel(PubRel {
+                                packet_identifier: todo!(),
+                                reason_code: todo!(),
+                                other_properties: todo!(),
+                            }),
+                        }
+                    }
+
+                    ConnectedChannelsOutgoingPacket::SubscriptionRequest(sub_req) => {
+                        let packet_identifier = todo!();
+                        match sub_req {
+                            SubscriptionRequest::Subscribe(notifier, ..) => {
+                                self.inflight.subscribe.insert(packet_identifier, notifier);
+                                Packet::Subscribe(Subscribe {
+                                    packet_identifier,
+                                    subscribe_to: todo!(),
+                                    other_properties: todo!(),
+                                })
+                            }
+
+                            SubscriptionRequest::Unsubscribe(notifier, ..) => {
+                                self.inflight
+                                    .unsubscribe
+                                    .insert(packet_identifier, notifier);
+                                Packet::Unsubscribe(Unsubscribe {
+                                    packet_identifier,
+                                    unsubscribe_from: todo!(),
+                                    other_properties: todo!(),
+                                })
+                            }
+                        }
+                    }
+
+                    ConnectedChannelsOutgoingPacket::PublishRequest(publish) => {
+                        let packet = match publish {
+                            PublishRequest::PublishQoS0(..) => Publish {
+                                topic_name: todo!(),
+                                packet_identifier_dup_qos: PacketIdentifierDupQoS::AtMostOnce,
+                                retain: todo!(),
+                                payload: todo!(),
+                                other_properties: todo!(),
+                            },
+
+                            PublishRequest::PublishQoS1(..) => {
+                                // TODO: Push to outgoing messages queue if packet ID can't be assigned.
+                                let packet_identifier = todo!();
+                                Publish {
+                                    topic_name: todo!(),
+                                    packet_identifier_dup_qos: PacketIdentifierDupQoS::AtLeastOnce(
+                                        packet_identifier,
+                                        todo!(),
+                                    ),
+                                    retain: todo!(),
+                                    payload: todo!(),
+                                    other_properties: todo!(),
+                                }
+                            }
+
+                            PublishRequest::PublishQoS2(..) => {
+                                // TODO: Push to outgoing messages queue if packet ID can't be assigned.
+                                let packet_identifier = todo!();
+                                Publish {
+                                    topic_name: todo!(),
+                                    packet_identifier_dup_qos: PacketIdentifierDupQoS::ExactlyOnce(
+                                        packet_identifier,
+                                        todo!(),
+                                    ),
+                                    retain: todo!(),
+                                    payload: todo!(),
+                                    other_properties: todo!(),
+                                }
+                            }
+                        };
+                        Packet::Publish(packet)
+                    }
+
+                    ConnectedChannelsOutgoingPacket::PingReq => Packet::PingReq(PingReq),
+                };
+                Some(packet)
+            }
+        }
     }
 
     /// Complete an in-flight operation with a received acknowledgement.
@@ -101,10 +242,14 @@ impl<S: Shared> Session<S> {
                     #[allow(clippy::collapsible_if)] // TODO: remove
                     if connack.is_success() {
                         self.connected = true;
+                        // TODO: Get PINGREQ duration from connect properties
+                        self.ch.pingreq = Some(PingReqTimer::new(Duration::from_secs(5)));
                     }
                     // TODO: Convert connack to user-facing type and complete notifier
                     //let connack = connack.into()
                     //notifier.complete(connack);
+                } else {
+                    todo!("treat as protocol error: unexpected CONNACK");
                 }
             }
             CompletedOperation::Subscribe(suback) => {
@@ -130,11 +275,10 @@ impl<S: Shared> Session<S> {
 
         // Set connection state
         self.connected = false;
+        self.ch.pingreq = None;
         // Remove and cancel any in-flight CONNECT
-        // This shouldn't happen, since DISCONNECT requires an existing connection to be issued.
         if let Some(notifier) = self.inflight.connect.take() {
             let _ = notifier.cancel();
-            log::warn!("Received DISCONNECT while CONNECT packet in-flight");
         }
         // Remove and cancel all in-flight SUBSCRIBEs
         for (pkid, notifier) in self.inflight.subscribe.drain() {
@@ -170,7 +314,46 @@ impl<S: Shared> Session<S> {
     #[allow(clippy::needless_pass_by_value)] //TODO: Remove
     #[allow(clippy::unused_self)]
     pub fn server_disconnect(&mut self, disconnect: Disconnect<S>) {
-        unimplemented!()
+        // NOTE: When we cancel CompletionNotifiers here, we don't care about the Result because
+        // if it fails, that just means the user no longer has the corresponding CompletionToken
+
+        // Set connection state
+        self.connected = false;
+        self.ch.pingreq = None;
+        // Remove and cancel any in-flight CONNECT
+        // This shouldn't happen, since DISCONNECT requires an existing connection to be issued.
+        if let Some(notifier) = self.inflight.connect.take() {
+            let _ = notifier.cancel();
+            log::warn!("Received DISCONNECT while CONNECT packet in-flight");
+        }
+        // Remove and cancel all in-flight SUBSCRIBEs
+        for (pkid, notifier) in self.inflight.subscribe.drain() {
+            let _ = notifier.cancel();
+            self.pkid_pool.release_pkid(pkid);
+        }
+        // Remove and cancel all in-flight UNSUBSCRIBEs
+        for (pkid, notifier) in self.inflight.unsubscribe.drain() {
+            let _ = notifier.cancel();
+            self.pkid_pool.release_pkid(pkid);
+        }
+
+        // If session is ended, additional state changes must be taken
+        if let Some(SessionExpiryInterval::Duration(0)) =
+            disconnect.other_properties.session_expiry_interval
+        {
+            // Remove and cancel all in-flight QoS 1 PUBLISHes
+            for (pkid, (_, notifier)) in self.inflight.publish_qos1.drain() {
+                let _ = notifier.cancel();
+                self.pkid_pool.release_pkid(pkid);
+            }
+            // Remove and cancel all in-flight QoS 2 PUBLISHes
+            for (pkid, (_, notifier)) in self.inflight.publish_qos2.drain() {
+                let _ = notifier.cancel();
+                self.pkid_pool.release_pkid(pkid);
+            }
+
+            // TODO: PUBREL, PUBREC, PUBCOMP
+        }
     }
 
     /// Trigger a disconnect and adjust state based on the error from the underlying transport
@@ -222,6 +405,57 @@ struct Channels {
     ack_rx: Receiver<AcknowledgementRequest>,
     /// Channel for sending incoming PUBLISH requests
     i_pub_tx: Sender<IncomingPublish>,
+    pingreq: Option<PingReqTimer>,
+}
+
+enum ConnectedChannelsOutgoingPacket {
+    AcknowledgementRequest(AcknowledgementRequest),
+    SubscriptionRequest(SubscriptionRequest),
+    PublishRequest(PublishRequest),
+    PingReq,
+}
+
+fn poll_connected_channels(
+    ch: &mut Channels,
+) -> impl Future<Output = ConnectedChannelsOutgoingPacket> {
+    futures_util::future::poll_fn(|cx| {
+        // Poll for outgoing ACKs, then for outgoing SUBSCRIBEs, then for outgoing PUBLISHes.
+        // If any of them yields an item, reset the PINGREQ timer, else poll for outgoing PINGREQs.
+
+        if let Poll::Ready(Some(ack_req)) = ch.ack_rx.poll_recv(cx) {
+            if let Some(pingreq) = &mut ch.pingreq {
+                pingreq.reset();
+            }
+            return Poll::Ready(ConnectedChannelsOutgoingPacket::AcknowledgementRequest(
+                ack_req,
+            ));
+        }
+
+        if let Poll::Ready(Some(sub_req)) = ch.sub_rx.poll_recv(cx) {
+            if let Some(pingreq) = &mut ch.pingreq {
+                pingreq.reset();
+            }
+            return Poll::Ready(ConnectedChannelsOutgoingPacket::SubscriptionRequest(
+                sub_req,
+            ));
+        }
+
+        if let Poll::Ready(Some(publish)) = ch.o_pub_rx.poll_recv(cx) {
+            if let Some(pingreq) = &mut ch.pingreq {
+                pingreq.reset();
+            }
+            return Poll::Ready(ConnectedChannelsOutgoingPacket::PublishRequest(publish));
+        }
+
+        if let Some(pingreq) = &mut ch.pingreq
+            && let Poll::Ready(()) = Pin::new(&mut *pingreq).poll(cx)
+        {
+            pingreq.reset();
+            return Poll::Ready(ConnectedChannelsOutgoingPacket::PingReq);
+        }
+
+        Poll::Pending
+    })
 }
 
 /// Contains data related to in-flight operations pending a response
@@ -267,5 +501,33 @@ where
             pubrec: HashMap::new(),
             pubrel: HashMap::new(),
         }
+    }
+}
+
+struct PingReqTimer {
+    inner: Pin<Box<Sleep>>,
+    duration: Duration,
+}
+
+impl PingReqTimer {
+    fn new(duration: Duration) -> Self {
+        Self {
+            inner: Box::pin(tokio::time::sleep(duration)),
+            duration,
+        }
+    }
+
+    fn reset(&mut self) {
+        self.inner
+            .as_mut()
+            .reset(tokio::time::Instant::now() + self.duration);
+    }
+}
+
+impl Future for PingReqTimer {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.inner.as_mut().poll(cx)
     }
 }
