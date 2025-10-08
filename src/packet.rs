14679ab32a960955474d5e05cb8c0f58b2f63d40
Carter Tinney <catinney@microsoft.com>
2025-10-08 12:44:52 -0700

    wip

--- src/packet.rs
+++ src/packet.rs
@@ -15,8 +15,20 @@ use bytes::Bytes;
 
 use crate::error::OperationFailure;
 use crate::{buffer_pool, mqtt_proto};
-pub use crate::mqtt_proto::PacketIdentifier;
+pub use crate::mqtt_proto::{PacketIdentifier, PacketIdentifierDupQoS};  // TODO: repalce instead of re-export
 use crate::topic::TopicName;
+
+/// Trait for converting a packet to a buffer-backed internal variant.
+// TODO: Should this be here, or will it make more sense to implement as functions in some kind of
+// conversion module? Depends on where and how the boundary between public and internal types works
+trait IntoBuffered<O>
+where
+    O: buffer_pool::Owned
+{
+    type BufferBacked;
+    fn into_buffered(self, owned: &mut O) -> Result<Self::BufferBacked, buffer_pool::Error>;
+}
+
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum QoS {
@@ -56,19 +68,31 @@ impl ConnAck {
     }
 }
 
+/////////////////////////////////////////////////////////////////////
 
-
-
-
-// TODO: Implement
 #[derive(Debug, Clone)]
 pub struct Publish {
-    pub topic_name: TopicName,
     pub payload: Bytes,
-    pub qos: QoS,
+    pub qos: PacketIdentifierDupQoS,    // TODO: Represent this better (DeliveryQoS enum with DeliveryInfo inside?)
+    pub retain: bool,
+    pub topic_name: TopicName,
     pub properties: PublishProperties,
 }
 
+impl <S> From<mqtt_proto::Publish<S>> for Publish
+where
+    S: buffer_pool::Shared
+{
+    fn from(value: mqtt_proto::Publish<S>) -> Publish {
+        Publish {
+            payload: Bytes::copy_from_slice(value.payload.as_ref()),
+            qos: value.packet_identifier_dup_qos,
+            retain: value.retain,
+            topic_name: value.topic_name.to_owned().into(),
+            properties: value.other_properties.into(),
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct PublishProperties {
@@ -118,11 +142,289 @@ where
     }
 }
 
+impl <O> IntoBuffered<O> for PublishProperties
+where
+    O: buffer_pool::Owned
+{
+    type BufferBacked = mqtt_proto::PublishOtherProperties<O::Shared>;
 
+    fn into_buffered(self, owned: &mut O) -> Result<Self::BufferBacked, buffer_pool::Error> {
+        Ok(mqtt_proto::PublishOtherProperties {
+            payload_is_utf8: matches!(self.payload_format_indicator, PayloadFormatIndicator::UTF8),
+            message_expiry_interval: self.message_expiry_interval,
+            topic_alias: self.topic_alias,
+            response_topic: self.response_topic.map(|t|t.into_inner().to_shared(owned)).transpose()?,
+            correlation_data: self.correlation_data.map(|b| mqtt_proto::BinaryData::new(owned, b)).transpose()?,
+            user_properties: map_user_properties_to_bytestr(owned, self.user_properties)?,
+            subscription_identifiers: self.subscription_identifiers,
+            content_type: self.content_type.map(|s| mqtt_proto::ByteStr::new(owned, s)).transpose()?,
+        })
+    }
+}
 
-// NOTE: These are aliased for clarity on specific packet types
-pub type PubAckProperties = AckProperties; // For QoS 1
-pub type PubRecProperties = AckProperties; // For QoS 2
+#[derive(Debug, Clone)]
+pub struct PubAck {
+    pub packet_identifier: PacketIdentifier,
+    pub reason: PubAckReason,
+    pub properties: PubAckProperties,
+}
+
+impl PubAck {
+    pub fn is_success(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn as_result(&self) -> Result<(), OperationFailure> {
+        unimplemented!()
+    }
+}
+
+impl <S> From<mqtt_proto::PubAck<S>> for PubAck
+where
+    S: buffer_pool::Shared
+{
+    fn from(value: mqtt_proto::PubAck<S>) -> PubAck {
+        PubAck {
+            packet_identifier: value.packet_identifier,
+            reason: value.reason_code.into(),
+            properties: value.other_properties.into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PubAckReason {
+    // Ok
+    Success = 0x00,
+    NoMatchingSubscribers = 0x10,
+    // Errors
+    UnspecifiedError = 0x80,
+    ImplementationSpecificError = 0x83,
+    NotAuthorized = 0x87,
+    TopicNameInvalid = 0x90,
+    PacketIdentifierInUse = 0x91,
+    QuotaExceeded = 0x97,
+    PayloadFormatInvalid = 0x99,
+}
+
+impl From<mqtt_proto::PubAckReasonCode> for PubAckReason
+{
+    fn from(value: mqtt_proto::PubAckReasonCode) -> PubAckReason {
+        match value {
+            mqtt_proto::PubAckReasonCode::Success => PubAckReason::Success,
+            mqtt_proto::PubAckReasonCode::NoMatchingSubscribers => PubAckReason::NoMatchingSubscribers,
+            mqtt_proto::PubAckReasonCode::UnspecifiedError => PubAckReason::UnspecifiedError,
+            mqtt_proto::PubAckReasonCode::ImplementationSpecificError => PubAckReason::ImplementationSpecificError,
+            mqtt_proto::PubAckReasonCode::NotAuthorized => PubAckReason::NotAuthorized,
+            mqtt_proto::PubAckReasonCode::TopicNameInvalid => PubAckReason::TopicNameInvalid,
+            mqtt_proto::PubAckReasonCode::PacketIdentifierInUse => PubAckReason::PacketIdentifierInUse,
+            mqtt_proto::PubAckReasonCode::QuotaExceeded => PubAckReason::QuotaExceeded,
+            mqtt_proto::PubAckReasonCode::PayloadFormatInvalid => PubAckReason::PayloadFormatInvalid,
+        }
+    }
+}
+
+impl From<PubAckReason> for mqtt_proto::PubAckReasonCode {
+    fn from(value: PubAckReason) -> mqtt_proto::PubAckReasonCode {
+        match value {
+            PubAckReason::Success => mqtt_proto::PubAckReasonCode::Success,
+            PubAckReason::NoMatchingSubscribers => mqtt_proto::PubAckReasonCode::NoMatchingSubscribers,
+            PubAckReason::UnspecifiedError => mqtt_proto::PubAckReasonCode::UnspecifiedError,
+            PubAckReason::ImplementationSpecificError => mqtt_proto::PubAckReasonCode::ImplementationSpecificError,
+            PubAckReason::NotAuthorized => mqtt_proto::PubAckReasonCode::NotAuthorized,
+            PubAckReason::TopicNameInvalid => mqtt_proto::PubAckReasonCode::TopicNameInvalid,
+            PubAckReason::PacketIdentifierInUse => mqtt_proto::PubAckReasonCode::PacketIdentifierInUse,
+            PubAckReason::QuotaExceeded => mqtt_proto::PubAckReasonCode::QuotaExceeded,
+            PubAckReason::PayloadFormatInvalid => mqtt_proto::PubAckReasonCode::PayloadFormatInvalid,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct PubAckProperties {
+    pub reason_string: Option<String>,
+    pub user_properties: Vec<(String, String)>,
+}
+
+impl <S> From<mqtt_proto::PubAckOtherProperties<S>> for PubAckProperties
+where
+    S: buffer_pool::Shared
+{
+    fn from(value: mqtt_proto::PubAckOtherProperties<S>) -> PubAckProperties {
+        PubAckProperties {
+            reason_string: value.reason_string.map(|s| s.to_string()),
+            user_properties: value.user_properties.into_iter().map(|(k, v)| (k.to_string(), v.to_string())).collect(),
+        }
+    }
+}
+
+impl <O> IntoBuffered<O> for PubAckProperties
+where
+    O: buffer_pool::Owned
+{
+    type BufferBacked = mqtt_proto::PubAckOtherProperties<O::Shared>;
+
+    fn into_buffered(self, owned: &mut O) -> Result<Self::BufferBacked, buffer_pool::Error> {
+        Ok(mqtt_proto::PubAckOtherProperties {
+            reason_string: self.reason_string.map(|s| mqtt_proto::ByteStr::new(owned, s)).transpose()?,
+            user_properties: map_user_properties_to_bytestr(owned, self.user_properties)?,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PubRec {
+    pub packet_identifier: PacketIdentifier,
+    pub reason: PubRecReason,
+    pub properties: PubRecProperties,
+}
+
+impl PubRec {
+    pub fn is_success(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub fn as_result(&self) -> Result<(), OperationFailure> {
+        unimplemented!()
+    }
+}
+
+impl <S> From<mqtt_proto::PubRec<S>> for PubRec
+where
+    S: buffer_pool::Shared
+{
+    fn from(value: mqtt_proto::PubRec<S>) -> PubRec {
+        PubRec {
+            packet_identifier: value.packet_identifier,
+            reason: value.reason_code.into(),
+            properties: value.other_properties.into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PubRecReason {
+    // Ok
+    Success = 0x00,
+    NoMatchingSubscribers = 0x10,
+    // Errors
+    UnspecifiedError = 0x80,
+    ImplementationSpecificError = 0x83,
+    NotAuthorized = 0x87,
+    TopicNameInvalid = 0x90,
+    PacketIdentifierInUse = 0x91,
+    QuotaExceeded = 0x97,
+    PayloadFormatInvalid = 0x99,
+}
+
+impl From<mqtt_proto::PubRecReasonCode> for PubRecReason
+{
+    fn from(value: mqtt_proto::PubRecReasonCode) -> PubRecReason {
+        match value {
+            mqtt_proto::PubRecReasonCode::Success => PubRecReason::Success,
+            mqtt_proto::PubRecReasonCode::NoMatchingSubscribers => PubRecReason::NoMatchingSubscribers,
+            mqtt_proto::PubRecReasonCode::UnspecifiedError => PubRecReason::UnspecifiedError,
+            mqtt_proto::PubRecReasonCode::ImplementationSpecificError => PubRecReason::ImplementationSpecificError,
+            mqtt_proto::PubRecReasonCode::NotAuthorized => PubRecReason::NotAuthorized,
+            mqtt_proto::PubRecReasonCode::TopicNameInvalid => PubRecReason::TopicNameInvalid,
+            mqtt_proto::PubRecReasonCode::PacketIdentifierInUse => PubRecReason::PacketIdentifierInUse,
+            mqtt_proto::PubRecReasonCode::QuotaExceeded => PubRecReason::QuotaExceeded,
+            mqtt_proto::PubRecReasonCode::PayloadFormatInvalid => PubRecReason::PayloadFormatInvalid,
+        }
+    }
+}
+
+impl From<PubRecReason> for mqtt_proto::PubRecReasonCode {
+    fn from(value: PubRecReason) -> mqtt_proto::PubRecReasonCode {
+        match value {
+            PubRecReason::Success => mqtt_proto::PubRecReasonCode::Success,
+            PubRecReason::NoMatchingSubscribers => mqtt_proto::PubRecReasonCode::NoMatchingSubscribers,
+            PubRecReason::UnspecifiedError => mqtt_proto::PubRecReasonCode::UnspecifiedError,
+            PubRecReason::ImplementationSpecificError => mqtt_proto::PubRecReasonCode::ImplementationSpecificError,
+            PubRecReason::NotAuthorized => mqtt_proto::PubRecReasonCode::NotAuthorized,
+            PubRecReason::TopicNameInvalid => mqtt_proto::PubRecReasonCode::TopicNameInvalid,
+            PubRecReason::PacketIdentifierInUse => mqtt_proto::PubRecReasonCode::PacketIdentifierInUse,
+            PubRecReason::QuotaExceeded => mqtt_proto::PubRecReasonCode::QuotaExceeded,
+            PubRecReason::PayloadFormatInvalid => mqtt_proto::PubRecReasonCode::PayloadFormatInvalid,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct PubRecProperties {
+    pub reason_string: Option<String>,
+    pub user_properties: Vec<(String, String)>,
+}
+
+impl <S> From<mqtt_proto::PubRecOtherProperties<S>> for PubRecProperties
+where
+    S: buffer_pool::Shared
+{
+    fn from(value: mqtt_proto::PubRecOtherProperties<S>) -> PubRecProperties {
+        PubRecProperties {
+            reason_string: value.reason_string.map(|s| s.to_string()),
+            user_properties: value.user_properties.into_iter().map(|(k, v)| (k.to_string(), v.to_string())).collect(),
+        }
+    }
+}
+
+impl <O> IntoBuffered<O> for PubRecProperties
+where
+    O: buffer_pool::Owned
+{
+    type BufferBacked = mqtt_proto::PubRecOtherProperties<O::Shared>;
+
+    fn into_buffered(self, owned: &mut O) -> Result<Self::BufferBacked, buffer_pool::Error> {
+        Ok(mqtt_proto::PubRecOtherProperties {
+            reason_string: self.reason_string.map(|s| mqtt_proto::ByteStr::new(owned, s)).transpose()?,
+            user_properties: map_user_properties_to_bytestr(owned, self.user_properties)?,
+        })
+    }
+}
+
+// NOTE: strict subset of PubAckReason/PubRecReason
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PubRejectReason {
+    UnspecifiedError = 0x80,
+    ImplementationSpecificError = 0x83,
+    NotAuthorized = 0x87,
+    TopicNameInvalid = 0x90,
+    PacketIdentifierInUse = 0x91,
+    QuotaExceeded = 0x97,
+    PayloadFormatInvalid = 0x99,
+}
+
+impl From<PubRejectReason> for mqtt_proto::PubAckReasonCode {
+    fn from(value: PubRejectReason) -> mqtt_proto::PubAckReasonCode {
+        match value {
+            PubRejectReason::UnspecifiedError => mqtt_proto::PubAckReasonCode::UnspecifiedError,
+            PubRejectReason::ImplementationSpecificError => mqtt_proto::PubAckReasonCode::ImplementationSpecificError,
+            PubRejectReason::NotAuthorized => mqtt_proto::PubAckReasonCode::NotAuthorized,
+            PubRejectReason::TopicNameInvalid => mqtt_proto::PubAckReasonCode::TopicNameInvalid,
+            PubRejectReason::PacketIdentifierInUse => mqtt_proto::PubAckReasonCode::PacketIdentifierInUse,
+            PubRejectReason::QuotaExceeded => mqtt_proto::PubAckReasonCode::QuotaExceeded,
+            PubRejectReason::PayloadFormatInvalid => mqtt_proto::PubAckReasonCode::PayloadFormatInvalid,
+        }
+    }
+}
+
+impl From<PubRejectReason> for mqtt_proto::PubRecReasonCode {
+    fn from(value: PubRejectReason) -> mqtt_proto::PubRecReasonCode {
+        match value {
+            PubRejectReason::UnspecifiedError => mqtt_proto::PubRecReasonCode::UnspecifiedError,
+            PubRejectReason::ImplementationSpecificError => mqtt_proto::PubRecReasonCode::ImplementationSpecificError,
+            PubRejectReason::NotAuthorized => mqtt_proto::PubRecReasonCode::NotAuthorized,
+            PubRejectReason::TopicNameInvalid => mqtt_proto::PubRecReasonCode::TopicNameInvalid,
+            PubRejectReason::PacketIdentifierInUse => mqtt_proto::PubRecReasonCode::PacketIdentifierInUse,
+            PubRejectReason::QuotaExceeded => mqtt_proto::PubRecReasonCode::QuotaExceeded,
+            PubRejectReason::PayloadFormatInvalid => mqtt_proto::PubRecReasonCode::PayloadFormatInvalid,
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////
+
+// TODO: Implement
 
 #[derive(Debug, Clone)]
 pub struct PubRelProperties {}
@@ -140,39 +442,6 @@ impl Default for PubCompProperties {
     }
 }
 
-
-#[derive(Debug, Clone)]
-pub struct PubAck {
-    pub reason: PubAckReason,
-    pub properties: PubAckProperties,
-}
-
-impl PubAck {
-    pub fn is_success(&self) -> bool {
-        unimplemented!()
-    }
-
-    pub fn as_result(&self) -> Result<(), OperationFailure> {
-        unimplemented!()
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct PubRec {
-    pub reason: PubRecReason,
-    pub properties: PubRecProperties,
-}
-
-impl PubRec {
-    pub fn is_success(&self) -> bool {
-        unimplemented!()
-    }
-
-    pub fn as_result(&self) -> Result<(), OperationFailure> {
-        unimplemented!()
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct PubRel {
     pub reason: PubRelReason,
@@ -184,12 +453,6 @@ pub struct PubComp {
     pub reason: PubCompReason,
     pub properties: PubCompProperties,
 }
-
-
-
-
-
-
 
 
 ////////////////////////////////////////////////////////////////////
@@ -224,14 +487,6 @@ pub struct AuthProperties {}
 impl Default for AuthProperties {
     fn default() -> Self {
         AuthProperties {}
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct AckProperties {}
-impl Default for AckProperties {
-    fn default() -> Self {
-        AckProperties {}
     }
 }
 
@@ -355,58 +610,6 @@ pub enum UnsubAckReason {
     PacketIdentifierInUse = 0x91,
 }
 
-// TODO: make this internal only
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum PubAckRecReason {
-    // Ok
-    Success = 0x00,
-    NoMatchingSubscribers = 0x10,
-    // Errors
-    UnspecifiedError = 0x80,
-    ImplementationSpecificError = 0x83,
-    NotAuthorized = 0x87,
-    TopicNameInvalid = 0x90,
-    PacketIdentifierInUse = 0x91,
-    QuotaExceeded = 0x97,
-    PayloadFormatInvalid = 0x99,
-}
-
-// NOTE: strict subset of PubAckReason/PubRecReason
-// TODO: Massage naming
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum PubRejectReason {
-    UnspecifiedError = 0x80,
-    ImplementationSpecificError = 0x83,
-    NotAuthorized = 0x87,
-    TopicNameInvalid = 0x90,
-    PacketIdentifierInUse = 0x91,
-    QuotaExceeded = 0x97,
-    PayloadFormatInvalid = 0x99,
-}
-
-// NOTE: We implement Into instead of From here because there isn't a non-failable conversion
-// the other way, as `PubRejectReason` is a strict subset of `PubAckRecReason`.
-#[allow(clippy::from_over_into)]
-impl Into<PubAckRecReason> for PubRejectReason {
-    fn into(self) -> PubAckRecReason {
-        match self {
-            PubRejectReason::UnspecifiedError => PubAckRecReason::UnspecifiedError,
-            PubRejectReason::ImplementationSpecificError => {
-                PubAckRecReason::ImplementationSpecificError
-            }
-            PubRejectReason::NotAuthorized => PubAckRecReason::NotAuthorized,
-            PubRejectReason::TopicNameInvalid => PubAckRecReason::TopicNameInvalid,
-            PubRejectReason::PacketIdentifierInUse => PubAckRecReason::PacketIdentifierInUse,
-            PubRejectReason::QuotaExceeded => PubAckRecReason::QuotaExceeded,
-            PubRejectReason::PayloadFormatInvalid => PubAckRecReason::PayloadFormatInvalid,
-        }
-    }
-}
-
-// NOTE: These are aliased for clarity on specific packet types
-pub type PubAckReason = PubAckRecReason; // For QoS 1
-pub type PubRecReason = PubAckRecReason; // For QoS 2
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PubRelReason {
     Success = 0x00,
@@ -430,3 +633,16 @@ pub enum AuthReason {
 
 // TODO: What about if you do get a subscription, but at a different QoS than you requested? success? failure?
 // Anything less than 0x80 is considered a success I think
+
+
+fn map_user_properties_to_bytestr<S, O>(owned: &mut O, props: Vec<(String, String)>) -> Result<Vec<(mqtt_proto::ByteStr<S>, mqtt_proto::ByteStr<S>)>, buffer_pool::Error>
+where
+    S: buffer_pool::Shared,
+    O: buffer_pool::Owned<Shared = S>,
+{
+    props.into_iter().map(|(k, v)| {
+        let k = mqtt_proto::ByteStr::new(owned, k)?;
+        let v = mqtt_proto::ByteStr::new(owned, v)?;
+        Ok((k, v))
+    }).collect()
+}
