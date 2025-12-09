8fa571fb44b1ed140af67842745dda82968ec9dd
Carter Tinney <catinney@microsoft.com>
2025-12-09 10:09:23 -0500

    Added missing punctuation in copyright headers (#71)

--- src/client/token.rs
+++ src/client/token.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License
+// Licensed under the MIT License.
 
 //! Token types for awaiting completion of MQTT operations and issuing acknowledgements.
 
--- src/packet.rs
+++ src/packet.rs
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License
+// Licensed under the MIT License.
 
 //! MQTT packet types and associated properties and reason codes.
 
